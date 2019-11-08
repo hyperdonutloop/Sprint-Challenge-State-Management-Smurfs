6fb6f4652d373f9ec29895d6d27b5fb4964db5d8
donutwizard666 <thebeardedwarrior@icloud.com>
2019-11-08 13:06:56 -0800

    added proper switch cases to reducer

--- smurfs/src/actions/index.jsx
+++ smurfs/src/actions/index.jsx
@@ -6,11 +6,9 @@ export const FETCH_SMURF_FAILED = 'FETCH_SMURF_FAILED';
 
 export const getSmurfs = () => dispatch => {
   dispatch({ type: FETCH_SMURF_LOADING });
-  axios.get('http://localhost:3333/smurfs')
-  .then(response => {
-    console.log(response);
-    return dispatch({ type: FETCH_SMURF_SUCCESS, payload: response.data })
-  })
+  axios
+    .get('http://localhost:3333/smurfs')
+    .then(response => dispatch({ type: FETCH_SMURF_SUCCESS, payload: response.data }))
     .catch(error => dispatch({ type: FETCH_SMURF_FAILED, payload: error }));
 }
 
--- smurfs/src/reducers/reducer.jsx
+++ smurfs/src/reducers/reducer.jsx
@@ -2,7 +2,9 @@ import {
   FETCH_SMURF_LOADING, 
   FETCH_SMURF_SUCCESS, 
   FETCH_SMURF_FAILED,
-  GET_NEW_SMURF
+  GET_NEW_SMURF,
+  GET_SMURF_LOADING,
+  GET_NEW_SMURF_FAILED
 } from '../actions/index';
 
 const initialState = {
@@ -15,29 +17,18 @@ function reducer(state = initialState, action) {
   console.log('reducer', action)
   switch (action.type) {
     case FETCH_SMURF_LOADING:
-      return {
-        ...state,
-        isFetching: true,
-        error: null
-      };
+      return {...state, isFetching: true, error: null};
     case FETCH_SMURF_SUCCESS:
-      return {
-        ...state,
-        smurf: action.payload,
-        isFetching: false,
-        error: null
-      };
-      case FETCH_SMURF_FAILED:
-        return {
-          ...state,
-          error: action.payload
-        };
-      case GET_NEW_SMURF:
-        return {
-          ...state,
-          smurf: action.payload
-        }
-    default:
+      return {...state, smurf: action.payload, isFetching: false, error: null};
+    case FETCH_SMURF_FAILED:
+      return {...state, error: action.payload};
+    case GET_SMURF_LOADING:
+      return {...state, isFetching: false, error: null, smurf: []}
+    case GET_NEW_SMURF:
+      return {...state, smurf: action.payload}
+    case GET_NEW_SMURF_FAILED: 
+      return {...state, isFetching: false, error: action.payload, smurf: []}
+default:
       return state
   }
 }
