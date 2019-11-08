a3e944f2d5ed8c916f8183164301f1b2a6e843d8
donutwizard666 <thebeardedwarrior@icloud.com>
2019-11-08 14:00:05 -0800

    was able to get post request to work. when clicking on GET SMURFS it does get all smurfs and new added smurfs, and does display them, however a bug appears when clicking the SUBMIT button to actually add the smurf

--- smurfs/src/components/SmurfForm.jsx
+++ smurfs/src/components/SmurfForm.jsx
@@ -1,32 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { addSmurfs } from '../actions/index';
 import { connect } from 'react-redux';
 
-const AddSmurf = (props) => {
-  const newSmurf = e => {
-    e.preventDefault();
-    props.addSmurfs();
+const SmurfForm = (props) => {
+  const [values, setValues] = useState({
+    name: '',
+    age: '',
+    height: 0
+  })
 
+  const handleChange = e => setValues({...values, [e.target.name]: e.target.value});
+  
+  const handleSubmit = async e => {
+    e.preventDefault();
+    await props.addSmurfs(values);
+    props.history.push('/');
   }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <label>New Smurf Information</label>
       <input
         type="text"
         name="name"
+        onChange={handleChange}
         placeholder="name"
+        value={values.name}
       />
       <input
         type="text"
         name="age"
+        onChange={handleChange}
         placeholder="age"
+        value={values.age}
       />
       <input
         type="text"
         name="height"
+        onChange={handleChange}
         placeholder="height"
+        value={values.height}
       />
-      <button onClick={newSmurf}>Submit</button>
+      <button>Submit</button>
     </form>
   )
 }
@@ -34,11 +49,9 @@ const AddSmurf = (props) => {
 // export default AddSmurf;
 
 const mapStateToProps = state => ({
-  smurfs: state.smurf,
-  error: state.error,
+  smurfs: state.smurf.smurf
 })
 
-export default connect(
-  mapStateToProps,
-  { addSmurfs }
-)(AddSmurf);
+const mapDispatchToProps ={addSmurfs}
+
+export default connect(mapStateToProps,mapDispatchToProps)(SmurfForm);
